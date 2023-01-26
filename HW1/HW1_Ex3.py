032b9476036dd71f75d222ab1b2782f1c159643f
John Lomas <jslomas@ucdavis.edu>
2023-01-25 21:38:36 -0800

    planning hw1_ex3

--- HW1/HW1_Ex3.py
+++ HW1/HW1_Ex3.py
@@ -1,10 +1,28 @@
-import numpy
+import numpy as np
 
-def calcNormalEquation(x: numpy.matrix, y: numpy.matrix) -> numpy.array:
-	theta = numpy.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
+def calcNormalEquation(x: np.matrix, y: np.matrix) -> np.array:
+	theta = np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
 	return (theta)
+
+def distance(d1: np.array, d2: np.array) -> int:
+	# TODO: calculate distance in 256 
 
 def calcKNN():
 	#TODO
 
-	
+def importData():
+	# TODO: Import 2s and 3s
+	# import as matrix
+	# code 2 -> 0, 3 -> 1
+
+def computeError():
+
+
+def main():
+	# Import training data
+	# Train linear regression
+	# Classify training set by linear regression -> compute error
+	# Classify test set by linear regression -> compute error
+	# Classify training by k-NN (1, 3, 5, 7, 15) -> compute error
+	# Classify test set by k-NN (1, 3, 5, 7, 15) -> compute error
+	#
